b81312aca4ba6bfa1953f574da03e8b78e71860b
MongooseCodes <mthbarber@gmail.com>
2021-10-26 17:42:09 +0100

    add div and need to make function work with increasing score via javaScript

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+const newDiv = document.createElement("div")
  //Create an array for the 3 selections possible Rock, Paper and Scissors
 const answerArray = ['Rock', 'Paper', 'Scissors']
 
@@ -17,19 +17,41 @@ function computerPlay() {
 let computerSelection = computerPlay();
 let playerChoice = "";
 let playerChoiceUpper = playerChoice.toUpperCase();
+
+//put html elements into variables so they can be incremented by PlayRound function
+function computerScoreUpdate(){
+    let computerScore = document.getElementById('computer');
+    let computerNumber = computerScore.innerHTML;
+    computerNumber++;
+    computerScore = computerNumber;
+}
+
+    let userScore = document.getElementById('user');
+    let userNumber = userScore.innerHTML;
+
+
  //Values are compared to see who wins
   function playRound(playerChoiceUpper, computerSelection) {
     if (playerChoiceUpper === 'SCISSORS' && computerSelection === 'PAPER') {
+        userNumber++;
+        userScore = userNumber;
         return console.log("You win! Scissors beats Paper!")
     } else if (playerChoiceUpper === 'SCISSORS' && computerSelection === 'ROCK') {
+        computerScoreUpdate();
         return console.log("You lose! Rock beats Scissors!")
     } else if (playerChoiceUpper === 'ROCK' && computerSelection === 'SCISSORS') {
+        userNumber++;
+        userScore = userNumber;
         return console.log("You win! Rock beats Scissors")
     } else if (playerChoiceUpper === 'ROCK' && computerSelection === 'PAPER') {
+        computerScoreUpdate();
         return console.log("You lose! Paper beats Rock!")
     } else if (playerChoiceUpper === 'PAPER' && computerSelection === 'ROCK') {
+        userNumber++;
+        userScore = userNumber;
         return console.log("You win! Paper beats Rock!")
     } else if (playerChoiceUpper === 'PAPER' && computerSelection === 'SCISSORS') {
+        computerScoreUpdate();
         return console.log("You lose! Scissors beats Paper!")
     } else {
         return console.log("You both chose " + playerChoiceUpper + " so its a draw!")
@@ -44,23 +66,26 @@ let playerChoiceUpper = playerChoice.toUpperCase();
 // add functionality to buttons to make UI
 const rock = document.querySelector('#rock');
 rock.addEventListener('click', () => {
-    let playerChoice = "ROCK";
-    playRound(playerChoice,computerPlay());
+    let playerChoice = "Rock";
+    let playerChoiceUpper = playerChoice.toUpperCase();
+    playRound(playerChoiceUpper,computerPlay());
     //alert("You clicked Rock");
 });
 
 const paper = document.querySelector('#paper');
 paper.addEventListener('click', () => {
-    let playerChoice = "PAPER";
-    playRound(playerChoice, computerPlay());
+    let playerChoice = "Paper";
+    let playerChoiceUpper = playerChoice.toUpperCase();
+    playRound(playerChoiceUpper, computerPlay());
     //alert("You clicked Paper");
 });
     
 
 const scissors = document.querySelector('#scissors');
 scissors.addEventListener('click', () => {
-    let playerChoice = "SCISSORS";
-    playRound(playerChoice, computerPlay());
+    let playerChoice = "Scissors";
+    let playerChoiceUpper = playerChoice.toUpperCase();
+    playRound(playerChoiceUpper, computerPlay());
     //alert("You clicked Scissors");
 });
     
