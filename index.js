c27763106d68f2dcb6ce1768513127016299c4a3
MongooseCodes <mthbarber@gmail.com>
2021-10-26 14:43:14 +0100

    add button functionality

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-// add functionality to buttons to make UI
-document.getElementById("rock").addEventListener("click", e => {
-    playRound("rock",computerPlay());
-});
-
-document.getElementById("paper").addEventListener("click", e => {
-    playRound("paper", computerPlay());
-});
-
-
-document.getElementById("scissors").addEventListener("click", e => {
-    playRound("scissors", computerPlay());
-});
 
  //Create an array for the 3 selections possible Rock, Paper and Scissors
 const answerArray = ['Rock', 'Paper', 'Scissors']
@@ -26,73 +13,55 @@ function computerPlay() {
 
  //User chooses from Rock, Paper or Scissors
 
+ //Put comp answer into variable for Playround func
+let computerSelection = computerPlay();
+let playerChoice = "";
+let playerChoiceUpper = playerChoice.toUpperCase();
  //Values are compared to see who wins
-  function playRound(playerChoice, computerSelection) {
-    if (playerChoice.toUpperCase() === 'SCISSORS' && computerSelection === 'PAPER') {
-        return "You win! Scissors beats Paper!"
-    } else if (playerChoice.toUpperCase() === 'SCISSORS' && computerSelection === 'ROCK') {
-        return "You lose! Rock beats Scissors!"
-    } else if (playerChoice.toUpperCase() === 'ROCK' && computerSelection === 'SCISSORS') {
-        return "You win! Rock beats Scissors"
-    } else if (playerChoice.toUpperCase() === 'ROCK' && computerSelection === 'PAPER') {
-        return "You lose! Paper beats Rock!"
-    } else if (playerChoice.toUpperCase() === 'PAPER' && computerSelection === 'ROCK') {
-        return "You win! Paper beats Rock!"
-    } else if (playerChoice.toUpperCase() === 'PAPER' && computerSelection === 'SCISSORS') {
-        return "You lose! Scissors beats Paper!"
+  function playRound(playerChoiceUpper, computerSelection) {
+    if (playerChoiceUpper === 'SCISSORS' && computerSelection === 'PAPER') {
+        return console.log("You win! Scissors beats Paper!")
+    } else if (playerChoiceUpper === 'SCISSORS' && computerSelection === 'ROCK') {
+        return console.log("You lose! Rock beats Scissors!")
+    } else if (playerChoiceUpper === 'ROCK' && computerSelection === 'SCISSORS') {
+        return console.log("You win! Rock beats Scissors")
+    } else if (playerChoiceUpper === 'ROCK' && computerSelection === 'PAPER') {
+        return console.log("You lose! Paper beats Rock!")
+    } else if (playerChoiceUpper === 'PAPER' && computerSelection === 'ROCK') {
+        return console.log("You win! Paper beats Rock!")
+    } else if (playerChoiceUpper === 'PAPER' && computerSelection === 'SCISSORS') {
+        return console.log("You lose! Scissors beats Paper!")
     } else {
-        return "You both chose " + playerSelection + " so its a draw!"
+        return console.log("You both chose " + playerChoiceUpper + " so its a draw!")
         }
     }
 
+
+
+
+
+
+// add functionality to buttons to make UI
+const rock = document.querySelector('#rock');
+rock.addEventListener('click', () => {
+    let playerChoice = "ROCK";
+    playRound(playerChoice,computerPlay());
+    //alert("You clicked Rock");
+});
+
+const paper = document.querySelector('#paper');
+paper.addEventListener('click', () => {
+    let playerChoice = "PAPER";
+    playRound(playerChoice, computerPlay());
+    //alert("You clicked Paper");
+});
     
 
-//Create function game so that its a best of 5
-
-// temporary close off this function while adding buttons
-//function game() {
-    let computerScore = 0;
-    let userScore = 0;
-    for (gamesPlayed = 1; gamesPlayed <= 5; gamesPlayed++){
-        
-        let playerChoice = prompt("Please enter either Rock, Paper or Scissors", "").toUpperCase();
-        let gameResult = playRound(playerChoice, computerPlay());
-        //console.log("This is my console log" + gameResult);
-        
-        if (playerChoice != "PAPER" && playerChoice != "ROCK" && playerChoice != "SCISSORS") {
-            console.log("This is not an available choice");
-            return playerChoice = prompt("Please enter either Rock, Paper or Scissors", "");
-        }
-        
-        
-        if (gameResult.includes("win")) {
-            userScore += 1;
-            console.log("The result is " + gameResult + " User: " + userScore + " Computer: " + computerScore);           
-                
-        } else if (gameResult.includes("lose")) {
-            computerScore +=1;
-            console.log("The result is", gameResult + " User: " + userScore + " Computer: " + computerScore);
-                
-        } else if (gameResult.includes("draw")){
-            userScore +=1;
-            computerScore +=1;
-            console.log("It's a draw, as you both chose " + playerChoice + " User: " + userScore + " Computer: " + computerScore);            
-        
-        }
-        
-         
-         if (gamesPlayed === 5) {
-            if(userScore > computerScore) {
-                return console.log("You win with a score of " + userScore + " : " + computerScore);
-            } else if (userScore < computerScore) {
-                return console.log("You lose with a score of " + userScore + " : " + computerScore);
-            } else {
-                return console.log("Its a draw");
-            }
-          }
-    }
-
+const scissors = document.querySelector('#scissors');
+scissors.addEventListener('click', () => {
+    let playerChoice = "SCISSORS";
+    playRound(playerChoice, computerPlay());
+    //alert("You clicked Scissors");
+});
     
 
-//}
-  
